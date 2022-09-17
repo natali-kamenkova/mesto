3ee50aea7a9bcffb622af7d9598ff7874ddfe25b
Natali Kamenkova <natalikamenkova@gmail.com>
2022-09-17 12:37:24 +0300

    web

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import './index.css'
 import { Card } from "../components/Card.js";
 import { FormValidator } from "../components/FormValidator.js";
 import { profilePopup, cardPopup, imgPopup, profileEditBtn, cardPopupBtn, profileInputName, profileInputJob, profileNameSelector, profileJobSelector } from "../utils/constants.js";
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
   entry: { main: './src/pages/index.js' },
@@ -27,6 +28,23 @@ module.exports = {
         use: 'babel-loader',
         
         exclude: '/node_modules/'
+      },
+      {
+        
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        'postcss-loader'
+      ]
       }
     ]
   },
@@ -35,5 +53,6 @@ module.exports = {
     template: './src/index.html' 
   }),
   new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin()
   ]
 };
